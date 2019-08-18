dff3e2ed69f39f72c4fcdee44bd6dde946476556
emm2305 <50798469+emm2305@users.noreply.github.com>
2019-08-18 13:14:45 -0500

    tidying

--- fractionSketch.js
+++ fractionSketch.js
@@ -8,7 +8,6 @@ var angleNew;
 var angleLine = 0;
 var redDoneAngle;
 
-
 var xLocation2;
 var numerator2;
 var denominator2;
@@ -22,7 +21,6 @@ var setDraw2 = 0;
 var plus = 0;
 var printErrorGo = 0;
 var xloc;
-
 
 function setup() {
   var canvas  = createCanvas(400, 400);
@@ -53,15 +51,15 @@ function drawFractionOne() {
 
 function drawFractionTwo() {
 	numerator2 = document.fraction2.numerator2.value;
-	denominator2 = document.fraction2.denominator2.value;
-		if (360 % denominator2 == 0) {
-    angleOriginal2 = 360 / denominator2;
-    angleNew2 = 0;
-    setDraw2 = 1;
-    xLocation2 = 300;
+    denominator2 = document.fraction2.denominator2.value;
+	if (360 % denominator2 == 0) {
+    	angleOriginal2 = 360 / denominator2;
+    	angleNew2 = 0;
+   		setDraw2 = 1;
+    	xLocation2 = 300;
 		plus = 0;
 		printErrorGo = 0;
-    document.getElementById("combineFractions").style.display = "block";
+    	document.getElementById("combineFractions").style.display = "block";
 	} else {
 		setDraw2 = 0;
 		printErrorGo = 1;
@@ -86,19 +84,19 @@ function drawTwoCombined() {
 function combineFractions() {
     xLocation = 200;
     xLocation2 = 200;
-		plus = 1;
-		document.getElementById("helpText").innerHTML = "Can you figure out the total?  If not, why not?";
-		document.getElementById("needHintOne").style.display = "block";
+	plus = 1;
+	document.getElementById("helpText").innerHTML = "Can you figure out the total?  If not, why not?";
+	document.getElementById("needHintOne").style.display = "block";
 }
 
 function getHintOne() {
-		document.getElementById("needHintOne").style.display = "none";
-		document.getElementById("needHintTwo").style.display = "block";
+	document.getElementById("needHintOne").style.display = "none";
+	document.getElementById("needHintTwo").style.display = "block";
 }
 
 function getHintTwo() {
-		document.getElementById("needHintTwo").style.display = "none";
-		document.getElementById("needHintThree").style.display = "block";
+	document.getElementById("needHintTwo").style.display = "none";
+	document.getElementById("needHintThree").style.display = "block";
 }
 
 
@@ -120,12 +118,11 @@ function gcd_two_numbers(x, y) {
 
 function setNumerators(x) {
   	var multiplier = x / denominator;
-		var multiplier2 = x / denominator2;
-		numerator = numerator * multiplier2;
-		numerator2 = numerator2 * multiplier;
-		denominator = lcmFraction;
-		denominator2 = lcmFraction;
-
+	var multiplier2 = x / denominator2;
+	numerator = numerator * multiplier2;
+	numerator2 = numerator2 * multiplier;
+	denominator = lcmFraction;
+	denominator2 = lcmFraction;
 	}
 
 function getHintThree() {
@@ -134,19 +131,19 @@ function getHintThree() {
 	drawOneCombined();
 	drawTwoCombined();
 	document.getElementById("needHintThree").style.display = "none";
-  return lcm_two_numbers(denominator, denominator2);
+  	return lcm_two_numbers(denominator, denominator2);
 	}
 
 
 function draw() {
 	background(225,236,243);
 	if (printErrorGo == 1) {
-		  push();
-			textSize(14);
-			var errorText = "Please, use one of the following numbers in the denominator: 1, 2, 3, 4, 5, 6, 8, 9, 10, or 12.";
-			text(errorText,xloc,100,200,200);
-			pop();
-		}
+		push();
+		textSize(14);
+		var errorText = "Please, use one of the following numbers in the denominator: 1, 2, 3, 4, 5, 6, 8, 9, 10, or 12.";
+		text(errorText,xloc,100,200,200);
+		pop();
+	}
   //1st fraction
   if (setDraw == 1) {
     noFill();
@@ -165,27 +162,27 @@ function draw() {
           }
       }
       redDoneAngle = angleLine;
-      //draw empty sections
-			for (diff = 0; diff < (denominator - numerator); diff++) {
-        //draw 1 set of empty lines
-        for (e = 0; e < angleOriginal; e++) {
-          noStroke();
-          line(0, 0, ((radius*(cos(angleLine)))),((radius*(sin(angleLine)))));
-          angleLine += 1;
-          }
+    //draw empty sections
+	for (diff = 0; diff < (denominator - numerator); diff++) {
+    //draw 1 set of empty lines
+		for (e = 0; e < angleOriginal; e++) {
+    		noStroke();
+			line(0, 0, ((radius*(cos(angleLine)))),((radius*(sin(angleLine)))));
+			angleLine += 1;
+      	    }
         }
     }
-    //draw the red dividing lines
-    for (i = 0; i < denominator; i++) {
-      stroke(0);
-      line(0, 0, ((radius*(cos(angleNew)))), ((radius*(sin(angleNew)))));
-      angleNew = angleOriginal * (i + 2);
-      }
+    //draw the red dividing line
+	for (i = 0; i < denominator; i++) {
+		stroke(0);
+		line(0, 0, ((radius*(cos(angleNew)))), ((radius*(sin(angleNew)))));
+    	angleNew = angleOriginal * (i + 2);
+    }
     //draw outside red circle
     circle(0, 0, 2 * radius);
     pop();
     //write fraction
-		writeFraction(100, 100, numerator, denominator, 'red');
+	writeFraction(100, 100, numerator, denominator, 'red');
 }
 
   //2nd fraction
@@ -226,29 +223,29 @@ function draw() {
     //draw outside red circle
     circle(0, 0, 2 * radius);
     pop();
-		writeFraction(300, 300, numerator2, denominator2, 'blue');
+	writeFraction(300, 300, numerator2, denominator2, 'blue');
   }
   if (plus == 1) {
     line(190,70,210,70);
     line(200,60,200,80);
   }
 
-	// showme Fractions
-	if (setDraw == 3) {
-		background(225,236,243);
+// showme Fractions
+if (setDraw == 3) {
+	background(225,236,243);
     //write 1st fraction
   	writeFraction(100, 97, numerator, denominator, 'red');
     //write 2nd fraction
   	writeFraction(300, 298, numerator2, denominator2, 'blue');
-		//plus
+	//plus
     line(190,70,210,70);
     line(200,60,200,80);
-		// directions
-		textSize(12);
-		text("Change the numbers in the text boxes above", 50, 200);
-		text("to match these fractions", 50, 215);
-		text("Hit submit for each one, then add", 50, 230);
-	 }
+	// directions
+	textSize(12);
+	text("Change the numbers in the text boxes above", 50, 200);
+	text("to match these fractions", 50, 215);
+	text("Hit submit for each one, then add", 50, 230);
+	}
 }
 
 function writeFraction(xloc1, xloc2, num, den, color) {
